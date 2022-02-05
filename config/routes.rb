018c9127ff292d01734d276327289a4effca4f07
Julia <julia.sarandi@gmail.com>
2022-02-04 16:19:17 -0800

    Rename action and view template to 'index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/about', to: 'about#show'
+  get '/about', to: 'about#index'
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
