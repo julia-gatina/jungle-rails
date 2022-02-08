c10de1e0341edf6c08ce35d582720245f387ebc6
Julia <julia.sarandi@gmail.com>
2022-02-08 01:12:41 -0800

    Update session controller, fix  missing code for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
+
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def self.authenticate_with_credentials(email, password)
+
+    @email = email.downcase
+    @user = User.find_by_email(@email)
+
+    if BCrypt::Password.new(@user.password_digest) == password 
+      return @user
+    else
+      return nil
+  end
+end
+
+
 end
