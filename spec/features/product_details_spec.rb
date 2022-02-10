b973954e400ca4090ac98786dd9b14f6678f5202
Julia <julia.sarandi@gmail.com>
2022-02-09 22:42:13 -0800

    Implement tests for adding a product to cart

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
+RSpec.feature "ProductDetails", type: :feature, js: true do
 
   # SETUP
   before :each do
@@ -22,7 +22,7 @@ RSpec.feature "ProductDetails", type: :feature do
       first('.product').click_on('Details')
 
     # commented out b/c it's for debugging only
-    #save_and_open_screenshot
+    save_and_open_screenshot
 
     expect(page).to have_css 'article.product-detail', count: 1
   end
