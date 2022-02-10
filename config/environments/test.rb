63da3e6f348087336b323a834c3b7b89b8852df4
Julia <julia.sarandi@gmail.com>
2022-02-09 22:15:40 -0800

    Implement testing home page

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  config.secret_token = SecureRandom.hex(64)
+  config.secret_key_base = SecureRandom.hex(64)
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
